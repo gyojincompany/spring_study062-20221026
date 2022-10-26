fb9ab34f8112ad1c83d31b4995ebca59e67e85e3
gyojincompany <luckyred@gmail.com>
2022-10-26 11:35:54 +0900

    xml 안에 java 컨테이너 추가

--- src/main/java/com/gyojincompany/test/MainClass.java
+++ src/main/java/com/gyojincompany/test/MainClass.java
@@ -1,13 +1,16 @@
 package com.gyojincompany.test;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+//		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
 		Student student1 = ctx.getBean("student1",Student.class);
 		
 		System.out.println(student1.getName());//홍길동
@@ -15,6 +18,22 @@ public class MainClass {
 		System.out.println(student1.getHobbys());//[운동, 노래, 게임]
 		System.out.println(student1.getHeight());//180
 		System.out.println(student1.getWeight());//80
+		
+		Student student2 = ctx.getBean("student2",Student.class);
+		
+		System.out.println(student2.getName());//김유신
+		System.out.println(student2.getAge());//30
+		System.out.println(student2.getHobbys());//[운동, 노래, 게임]
+		System.out.println(student2.getHeight());//180
+		System.out.println(student2.getWeight());//80
+		
+		Student student3 = ctx.getBean("student3",Student.class);
+		
+		System.out.println(student3.getName());//강감찬
+		System.out.println(student3.getAge());//30
+		System.out.println(student3.getHobbys());//[운동, 노래, 게임]
+		System.out.println(student3.getHeight());//180
+		System.out.println(student3.getWeight());//80
 		
 	}
 
